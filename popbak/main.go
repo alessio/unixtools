86d9908f8188df39e9cf207681b9340b29fda678
Alessio Treglia <alessio@tendermint.com>
2020-10-19 19:22:00 +0100

    Backup tools (#1)

---
+++ popbak/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/alessio/tools/internal/dirsnapshots"
+)
+
+func main() {
+	log.SetPrefix("popbak: ")
+	log.SetFlags(0)
+	flag.Parse()
+
+	if flag.NArg() != 1 {
+		log.Fatalf("invalid arguments")
+	}
+
+	target, err := filepath.Abs(flag.Arg(0))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	backups, err := dirsnapshots.Load()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if err := restoreDirectory(target, backups); err != nil {
+		log.Fatalln(err)
+	}
+
+	if err := dirsnapshots.Save(backups); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func restoreDirectory(target string, backups *dirsnapshots.Backups) error {
+	orig, ok := backups.PopDir(target)
+	if !ok {
+		return fmt.Errorf("no backups available")
+	}
+
+	if err := os.RemoveAll(target); err != nil {
+		return fmt.Errorf("couldn't remove %q: %v", target, err)
+	}
+
+	if err := os.Rename(orig, target); err != nil {
+		return fmt.Errorf("couldn't rename %q: %v", orig, err)
+	}
+
+	return nil
+}
---
+++ pushbak/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/alessio/tools/internal/dirsnapshots"
+	"github.com/alessio/tools/internal/file"
+)
+
+var shelveMode bool
+
+func init() {
+	flag.BoolVar(&shelveMode, "-shelve", false, "shelve the directory once the backup copy is done")
+}
+
+func main() {
+	log.SetPrefix("pushbak: ")
+	log.SetFlags(0)
+	flag.Parse()
+
+	if flag.NArg() != 1 {
+		log.Fatalf("invalid arguments")
+	}
+
+	target, err := filepath.Abs(flag.Arg(0))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	backups, err := dirsnapshots.Load()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if err := backupDirectory(target, backups); err != nil {
+		log.Fatalln(err)
+	}
+
+	if err := dirsnapshots.Save(backups); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func backupDirectory(target string, backups *dirsnapshots.Backups) error {
+	backupDir, err := ioutil.TempDir(backups.SnapshotsDir(), "")
+	if err != nil {
+		return err
+	}
+
+	defer backups.PushDir(target, backupDir)
+
+	if shelveMode {
+		return os.Rename(target, backupDir)
+	}
+
+	if err := os.Remove(backupDir); err != nil {
+		return err
+	}
+
+	return file.CopyDir(target, backupDir)
+}
