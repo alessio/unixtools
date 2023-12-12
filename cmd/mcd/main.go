d54b4e32023124701a46044b7b73f38a7243a624
Alessio Treglia <al@essio.dev>
2023-12-12 23:41:17 +0100

    bugfixing

--- cmd/mcd/main.go
+++ cmd/mcd/main.go
@@ -3,33 +3,43 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/alessio/unixtools/internal/version"
 )
 
 var (
 	helpMode    bool
+	debugMode   bool
 	versionMode bool
-	mode        uint
+
+	dbgLog *log.Logger
 )
 
 func init() {
+	flag.CommandLine.SetOutput(os.Stderr)
+	flag.CommandLine.Usage = usage
+
 	flag.BoolVar(&helpMode, "help", false, "display this help and exit.")
 	flag.BoolVar(&versionMode, "version", false, "output version information and exit.")
+	flag.BoolVar(&debugMode, "D", false, "produce debug output.")
 
-	flag.Usage = usage
-	flag.ErrHelp = nil
+	log.SetFlags(0)
+	log.SetPrefix("mcd: ")
+
+	dbgLog = log.New(io.Discard, "mcd: ", log.LstdFlags)
 }
 
 func main() {
-	log.SetFlags(0)
-	log.SetPrefix("mcd: ")
-	log.SetOutput(os.Stderr)
 	flag.Parse()
 
 	handleHelpAndVersionModes()
+	if debugMode {
+		dbgLog.SetOutput(os.Stderr)
+	}
 
 	if _, err := os.Getwd(); err != nil {
 		log.Fatal(err)
@@ -39,12 +49,21 @@ func main() {
 		log.Fatal("invalid arguments")
 	}
 
-	if err := os.MkdirAll(flag.Arg(0), os.ModePerm); err != nil {
-		log.Fatal(err)
+	dbgLog.Printf("Abs(%s)\n", flag.Arg(0))
+	newDir, err := filepath.Abs(flag.Arg(0))
+	if err != nil {
+		log.Fatalf("couldn't find an absolute path: %v", err)
+	}
+	dbgLog.Println("Abs =", newDir)
+
+	if err := os.MkdirAll(newDir, os.ModePerm); err != nil {
+		dbgLog.Printf("mkdir: err = %v", err)
+		log.Fatalf("couldn't create the directory: %v", err)
 	}
 
-	if err := os.Chdir(flag.Arg(0)); err != nil {
-		log.Fatal(err)
+	if err := os.Chdir(newDir); err != nil {
+		dbgLog.Printf("chdir: err = %v", err)
+		log.Fatalf("couldn't cd: %v", err)
 	}
 }
 
@@ -52,11 +71,12 @@ func handleHelpAndVersionModes() {
 	switch {
 	case helpMode:
 		usage()
+		os.Exit(0)
 	case versionMode:
 		version.PrintWithCopyright()
+		os.Exit(0)
 	}
 
-	os.Exit(0)
 }
 
 func usage() {
